5140aaeb4e4cf4472d156bc0e09441b2a547fe40
alisitsa <andreylisitsa1982@gmail.com>
2016-11-26 16:10:42 +0200

    update

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -2,9 +2,20 @@ requirejs.config({
     baseUrl: './js',
 });
 
-define(["jquery", "domReady", "slick"], function ($) {
+define(["jquery", "domReady", "slick", 'select2'], function ($) {
     "use strict";
     $('.main-banner').slick();
+    $('#select-probe').select2({
+        placeholder: 'Выберите пробу',
+        minimumResultsForSearch: Infinity
+    });
+    $('.has-children').on('mouseenter', function(){
+        $(this).find('.submenu').stop().slideDown('fast').addClass('active');
+    });
+    $('.has-children').on('mouseleave', function(){
+        $(this).find('.submenu').stop().slideUp('fast').removeClass('active');
+    });
+    
 });
 
 
